c7c20f8c0e7241078b2f680036e474e876607a9e
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-07 17:17:35 +0200

    Initial Commit

---
+++ Bookworm-ios17/Bookworm_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm_ios17App.swift
+//  Bookworm-ios17
+//
+//  Created by Alin RADU on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
